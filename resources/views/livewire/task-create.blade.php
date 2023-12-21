e6c7a98d32566ee1ac83f4b5b6898fa41c1b4f32
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-21 17:51:54 +0100

    add logo and some tailwind style

--- resources/views/livewire/task-create.blade.php
+++ resources/views/livewire/task-create.blade.php
@@ -1,17 +1,37 @@
 <div>
+    <h2 class="text-center mb-3 text-lg">Add a new Task</h2>
     <form wire:submit='add'>
-        <label for="title">Title</label>
-        <input id="title" type="text" wire:model='task_title'>
-        <label for="description">Description</label>
-        <textarea cols="30" rows="10" wire:model='task_description' id="description"></textarea>
-        <select id="status" wire:model='task_status'>
-            @foreach (\App\Enums\Status::cases() as $status)
-                <option value="{{ $status->value }}">{{ $status->name }}</option>
-            @endforeach
+        <div class="flex m-auto w-2/4 gap-3">
+            <div>
+                <div class="flex flex-col">
+                    <label for="title">Title</label>
+                    <input class="rounded-md bg-gray-100" id="title" type="text" wire:model='task_title'>
+                </div>
+                <div class="flex flex-col">
+                    <label for="status">Status</label>
+                    <select class="rounded-md bg-gray-100" id="status" wire:model='task_status'>
+                        @foreach (\App\Enums\Status::cases() as $status)
+                            <option value="{{ $status->value }}">{{ $status->name }}</option>
+                        @endforeach
 
-        </select>
-        <label for="deadline">Deadline</label>
-        <input id="deadline" type="date" wire:model='task_deadline'>
-        <button type="submit">Add</button>
+                    </select>
+                </div>
+                <div class="flex flex-col">
+                    <label for="deadline">Deadline</label>
+                    <input class="rounded-md bg-gray-100" id="deadline" type="date" wire:model='task_deadline'>
+                </div>
+            </div>
+            <div>
+                <div class="flex flex-col">
+                    <label for="description">Description</label>
+                    <textarea class="rounded-md bg-gray-100" cols="30" rows="10" wire:model='task_description' id="description"></textarea>
+                </div>
+
+            </div>
+        </div>
+        <div class="flex justify-end w-2/4 m-auto">
+            <button class="bg-blue-500 py-2 px-4 text-white rounded-md" type="submit">Add</button>
+        </div>
+
     </form>
 </div>
